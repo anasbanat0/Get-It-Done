cf47e2053342fbb32fc7efe529279ee21ccf21cd
Anas Banat <anasbanat98banat@gmail.com>
2022-10-17 01:58:56 +0300

    Starting work with JS

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("task-input");
+const totalTasks = document.getElementById("total");
+const completedTasks = document.getElementById("completed");
+const modal = document.getElementById("modal");
+const maxRecentlyDeleted = 4;
